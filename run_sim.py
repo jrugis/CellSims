b40f262f8cbf4ba79f861037927a3fd32423273a
jrugis <j.rugis@auckland.ac.nc>
2016-06-20 19:08:48 +1200

    change scripts and naming to use all seven cells

--- run_sim.py
+++ run_sim.py
@@ -36,8 +36,7 @@ def replace_pvalue(p, v):
 ##################################################################
 
 # get the user set parameters
-platform, mesh, model, parms, parmA, valsA, parmB, valsB = _my_sweep.sweep_parms()
-print "Cell Simulation:", model, mesh
+platform, solver, mesh_type, model, parms, parmA, valsA, parmB, valsB = _my_sweep.sweep_parms()
 
 # create the top level results directory
 csdir = os.getcwd()
@@ -47,45 +46,53 @@ if not os.path.exists("results"):
 path = "results/CS" + time.strftime("%y%m%d%H%M%S")
 os.mkdir(path)
 os.chdir(path)
+model_full = model + "_" + platform + "_" + solver
+print "Cell Simulation:", model_full
 
-# iterate through the parameters
+# iterate through seven cells and all the parameters
+for cell in range(1, 8):
+  path = str(cell)
+  os.mkdir(path)
+  os.chdir(path)
+  mesh = "cell0" + str(cell) + mesh_type
+  print mesh
+  for v1 in valsA:
+    for v2 in valsB:
+      pdir = ""
+      #for pv in parms:
+      #  pdir += pv[0] + '-' + str(pv[1]) + '_'
+      pdir += parmA + '-' + str(v1) + '_' + parmB + '-' + str(v2)
+      pdir = pdir.replace('.', 'p')
+      os.mkdir(pdir)
+      os.chdir(pdir)
 
-for v1 in valsA:
-  for v2 in valsB:
-    pdir = ""
-    #for pv in parms:
-    #  pdir += pv[0] + '-' + str(pv[1]) + '_'
-    pdir += parmA + '-' + str(v1) + '_' + parmB + '-' + str(v2)
-    pdir = pdir.replace('.', 'p')
-    os.mkdir(pdir)
-    os.chdir(pdir)
+      # copy data files and execute the simulation
+      os.system("cp " + csdir + "/meshes/" + mesh + ".msh cs.msh")
+      os.system("cp " + csdir + "/parameters/" + model + ".dat cs.dat")
+              
+      for pv in parms: # set the fixed values
+        replace_pvalue(pv[0], pv[1])
+      replace_pvalue(parmA, v1) # set the swept values
+      replace_pvalue(parmB, v2)
 
-    # copy data files and execute the simulation
-    os.system("cp " + csdir + "/meshes/" + mesh + ".msh cs.msh")
-    os.system("cp " + csdir + "/parameters/" + model + ".dat cs.dat")
-            
-    for pv in parms: # set the fixed values
-      replace_pvalue(pv[0], pv[1])
-    replace_pvalue(parmA, v1) # set the swept values
-    replace_pvalue(parmB, v2)
+      if platform == "linux":
+          NP_MAX = 20
 
-    if platform == "linux":
-        NP_MAX = 12
+          # wait if at maximum number of parallel processes
+          while int(os.popen("ps | grep " + model + " | wc -l").read()) >= NP_MAX:
+              time.sleep(1)
 
-        # wait if at maximum number of parallel processes
-        while int(os.popen("ps | grep " + model + " | wc -l").read()) >= NP_MAX:
-            time.sleep(1)
+          # run a non-blocking simulation process
+          print(pdir)
+          os.system("bash " + csdir + "/linux.sh " + model_full + " " + model + " " + mesh + " " + csdir + " &")
+          time.sleep(0.5) # wait until after startup memory useage peak
+      elif platform == "pan":
+          os.system("sbatch " + csdir + "/run_sim.sl " + model_full + " " + model + " " + mesh + " " + csdir)
+      else:
+          print "ERROR: invalid platform -", platform    
 
-        # run a non-blocking simulation process
-        print(pdir)
-        os.system("bash " + csdir + "/linux.sh " + model + " " + mesh + " &")
-        time.sleep(0.5) # wait until after startup memory useage peak
-    elif platform == "pan":
-        os.system("sbatch " + csdir + "/run_sim.sl " + model + " " + mesh + " " + csdir)
-    else:
-        print "ERROR: platform not specified"    
-
-    os.chdir("..")
+      os.chdir("..")
+  os.chdir("..")
 
 # go back to top level
 os.chdir(csdir)
