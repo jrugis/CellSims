8d65e7dbd615d8eb843f036866d0820527e0d4bc
jrugis <j.rugis@auckland.ac.nc>
2016-06-21 16:33:15 +1200

    updated seven cell post processing

--- post/cs_results_R_cell.py
+++ post/cs_results_R_cell.py
@@ -6,8 +6,9 @@ import sys
 import cs
 
 results_dir = str(sys.argv[1])
-rdir = results_dir.split('/')[-2]    # results directory
-parms_dir = str(sys.argv[2])
+rdir = results_dir.split('/')[-1]    # results directory
+cell_dir = str(sys.argv[2])
+cell = "cell0" + cell_dir
 
 ##################################################################
 # main program
@@ -15,36 +16,59 @@ parms_dir = str(sys.argv[2])
 
 # navigate to the results directory
 path = os.getcwd()
-os.chdir(results_dir)
+os.chdir(results_dir + '/' + cell_dir)
+dirs = os.listdir('.')
+
+# identify the sweep parameters, assumed 2D
+sdir = dirs[0].split('_') # split directory
+bdir = ""                 # base directory
+for i in range(len(sdir) - 2): bdir += sdir[i] + '_' 
+
+varA = sdir[-2].split('-')[0]
+varB = sdir[-1].split('-')[0]
+valsA = []
+valsB = []
+for d in dirs:
+  valsA.append(float(d.split('_')[-2].split('-')[1].replace('p', '.')))
+  valsB.append(float(d.split('_')[-1].split('-')[1].replace('p', '.')))
+valsA = sorted(list(set(valsA)))
+valsB = sorted(list(set(valsB)))
+
+print valsA, valsB
+
+plt.ioff()
 
 # plot the calcium data
-rows = 7
-cols = 1
+cols = len(valsA)
+rows = len(valsB)
 plt.rcParams['axes.color_cycle'] = ['r', 'g', 'b']
-fig, plots = plt.subplots(rows, cols, sharex='col', sharey='col')
-fig.canvas.set_window_title(rdir) 
-fig.text(0.02, 0.96, rdir + "  " + parms_dir, fontsize=10)
-fig.set_size_inches(cols * 5, rows * 2)
-plots[rows-1].set_xlabel("time (s)")
-plots[rows-1].set_ylabel("CA+ concentration (uM)")
+fig, plots = plt.subplots(rows, cols, sharex='col', sharey='all')
+fig.canvas.set_window_title(rdir + "  " + cell) 
+fig.text(0.02, 0.96, rdir + "  " + cell, fontsize=10)
+fig.set_size_inches(cols * 3, rows * 3)
+plots[rows-1, 0].set_xlabel("time (s)")
+plots[rows-1, 0].set_ylabel("CA+ concentration (uM)")
 
 for i in range(rows):
-  d = str(i+1) + "/" + parms_dir
-  os.chdir(d)
+  for j in range(cols):
+    d  = varA + '-' + str(valsA[j]).replace('.', 'p') + '_'
+    d += varB + '-' + str(valsB[i]).replace('.', 'p')
+    os.chdir(bdir + d)
 
-  # get the simulation time scale
-  t_delta, t_end = cs.get_sim_time()
-  x = np.linspace(0.0, t_end, t_end/t_delta, endpoint=True)
+    # get the simulation time scale
+    t_delta, t_end = cs.get_sim_time()
+    x = np.linspace(0.0, t_end, t_end/t_delta, endpoint=True)
 
-  # plot the calcium concentration
-  plots[i].set_title("cell0" + str(i+1), fontsize=10)
-  if os.path.isfile("cR.bin"):
-    plots[i].plot(x, np.transpose(cs.get_data("cR.bin")), lw=0.5)
-  os.chdir('../..')
+    # plot the calcium concentration
+    plots[rows-1-i, j].set_title(d, fontsize=10)
+    if os.path.isfile("cR.bin"):
+        plots[rows-1-i, j].plot(x, np.transpose(cs.get_data("cR.bin")), lw=0.5)
+    os.chdir('..')
 
 open('../../temp.pdf', 'w').close()
 plt.savefig('../../temp.pdf')
-os.rename('../../temp.pdf', '../' + rdir + '.pdf')
+os.rename('../../temp.pdf', '../../' + rdir + "_" + cell + '.pdf')
+plt.draw()
 plt.show()
 
 # go back to top level
--- post/cs_results_R_parm.py
+++ post/cs_results_R_parm.py
@@ -19,14 +19,15 @@ os.chdir(results_dir)
 
 # plot the calcium data
 rows = 7
-cols = 1
+cols = 2
 plt.rcParams['axes.color_cycle'] = ['r', 'g', 'b']
 fig, plots = plt.subplots(rows, cols, sharex='col', sharey='col')
-fig.canvas.set_window_title(rdir) 
+fig.canvas.set_window_title(rdir + "  " + parms_dir) 
 fig.text(0.02, 0.96, rdir + "  " + parms_dir, fontsize=10)
-fig.set_size_inches(cols * 5, rows * 2)
-plots[rows-1].set_xlabel("time (s)")
-plots[rows-1].set_ylabel("CA+ concentration (uM)")
+fig.set_size_inches(cols * 10, rows * 2)
+plots[rows-1, 0].set_xlabel("time (s)")
+plots[rows-1, 0].set_ylabel("CA+ concentration (uM)")
+plots[rows-1, 1].set_ylabel("IP3 concentration (uM)")
 
 for i in range(rows):
   d = str(i+1) + "/" + parms_dir
@@ -37,14 +38,17 @@ for i in range(rows):
   x = np.linspace(0.0, t_end, t_end/t_delta, endpoint=True)
 
   # plot the calcium concentration
-  plots[i].set_title("cell0" + str(i+1), fontsize=10)
+  plots[i, 0].set_title("cell0" + str(i+1), fontsize=10)
+  plots[i, 1].set_title("cell0" + str(i+1), fontsize=10)
   if os.path.isfile("cR.bin"):
-    plots[i].plot(x, np.transpose(cs.get_data("cR.bin")), lw=0.5)
+    plots[i, 0].plot(x, np.transpose(cs.get_data("cR.bin")), lw=0.5)
+  if os.path.isfile("ip3R.bin"):
+    plots[i, 1].plot(x, np.transpose(cs.get_data("ip3R.bin")), lw=0.5)
   os.chdir('../..')
 
 open('../../temp.pdf', 'w').close()
 plt.savefig('../../temp.pdf')
-os.rename('../../temp.pdf', '../' + rdir + '.pdf')
+os.rename('../../temp.pdf', '../' + rdir + "_" + parms_dir + '.pdf')
 plt.show()
 
 # go back to top level
